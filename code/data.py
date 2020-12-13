fe3ccc113993e1b2de70744b9ea08b3edd6b6a9d
keya-desai <keyadesai97@gmail.com>
2020-12-13 16:53:27 +0530

    Update data.py to store training and testing data

--- code/data.py
+++ code/data.py
@@ -1,67 +1,113 @@
 import numpy as np 
 import pandas as pd 
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 
 class Data:
 
 	def __init__(self, dataPath):
 		self.path = dataPath
-		self.trainData = None
-		self.testData = None
+		self.data = None
+		# self.testData = None
 		self.dataWithNan = None
 		self.columnList = ['age', 'workclass', 'fnlwgt', 'education', 'educationNum', 'maritalStatus', 'occupation', 'relationship', 'race', 'sex', 'capitalGain', 'capitalLoss', 'hoursPerWeek', 'nativeCountry', 'income']
 		self.categoricalColumnList = ['workclass', 'education', 'maritalStatus', 'occupation', 'relationship', 'race', 'sex', 'nativeCountry', 'income']
 
 
-	def getData(self):
+	def getData(self, newData = False):
 
-		self.trainData = pd.read_csv(self.path + 'adult.data', header = None)
-		self.trainData.columns = self.columnList
+		if newData:
 
-		self.testData = pd.read_csv(self.path + 'adult.test', skiprows = 1, header = None)
-		self.testData.columns = self.columnList
+			self.data = pd.read_csv(self.path + 'adult.data', header = None)
+			self.data.columns = self.columnList
 
-		# Preprocessing Data
-		# Removing NaN values 
-		self.removeNan(self.trainData)
-		self.removeNan(self.testData)
+			# self.testData = pd.read_csv(self.path + 'adult.test', skiprows = 1, header = None)
+			# self.testData.columns = self.columnList
 
-		print("Train Data shape = ", self.trainData.shape)
-		print("Test Data shape = ", self.testData.shape)
-		print("Data with Nan shape = ", self.dataWithNan.shape)
+			# Preprocessing Data
+			# Removing NaN values 
+			self.removeNan(self.data)
+			# self.removeNan(self.testData)
 
-		# One hot encoding
-		self.trainData = self.oneHotEncoding(self.trainData)
-		# print(self.trainData.head())
-		# (30162, 105)
-		print(self.trainData.shape)
+			# One hot encoding
+			self.data = self.oneHotEncoding(self.data)
 
-		self.testData = self.oneHotEncoding(self.testData)
-		# print(self.testData.head())
-		# (15060, 104)
-		print(self.testData.shape)
+			print("Data shape = ", self.data.shape)
+			# print("Test Data shape = ", self.testData.shape)
+			print("Data with Nan shape = ", self.dataWithNan.shape)
 
-		# 1 column in missing in test data - add feature manually
-		for column in self.trainData.columns:
-			if column not in self.testData.columns:
-				print("Adding dummy column {} in Test data".format(column))
-				self.testData[column] = 0
-		print(self.testData.shape)
+			
+			# print(self.data.head())
+			# (30162, 105)
+			# self.data = self.data.head(1000)
+			print(self.data.shape)
 
-		X = self.trainData
-		X_prime, isFeatureReal = self.prepareMissingData(X)
-		print(isFeatureReal)
-		print(isFeatureReal.shape)
-		print("Total missing = ", np.sum(isFeatureReal==0))
+			# self.testData = self.oneHotEncoding(self.testData)
+			# print(self.testData.head())
+			# (15060, 104)
+			# print(self.testData.shape)
 
-		print(X_prime)
-		print(X_prime.shape)
+			# 1 column in missing in test data - add feature manually
+			# for column in self.data.columns:
+				# if column not in self.testData.columns:
+					# print("Adding dummy column {} in Test data".format(column))
+					# self.testData[column] = 0
+			# print(self.testData.shape)
 
-		# xTrain, yTrain = self.trainData.drop("income", axis = 1), self.trainData['income']
-		# xTest, yTest = self.testData.drop("income", axis = 1), self.testData['income']
+
+			X_train, X_test = train_test_split(self.data, test_size = 0.20, random_state = 42)
+			print(X_train.shape, X_test.shape)
+			# exit()
+			X_prime_train, isFeatureReal_train = self.prepareMissingData(X_train)
+			X_prime_test, isFeatureReal_test = self.prepareMissingData(X_test)
+
+			# Conver X_train and X_test to numpy
+			X_train = X_train.to_numpy()
+			X_test = X_test.to_numpy()
+
+			# X_prime, isFeatureReal = self.prepareMissingData(self.data)
+			# X_train, X_test, X_missing_train, X_missing_test = train_test_split(X, X_prime, test_size=0.20, random_state=42)
+
+
+
+			# with open(self.path + 'x.npy', 'wb') as f:
+			np.save(self.path + 'Train/X.npy', X_train)
+			np.save(self.path + 'Test/X.npy', X_test)
+			# with open(self.path + 'x_prime.npy', 'wb') as f:
+			np.save(self.path + 'Train/X_prime.npy', X_prime_train)
+			np.save(self.path + 'Test/X_prime.npy', X_prime_test)
+			# with open(self.path + 'feature_information.npy', 'wb') as f:
+			np.save(self.path + 'Train/feature_information.npy', isFeatureReal_train)
+			np.save(self.path + 'Test/feature_information.npy', isFeatureReal_test)
+
+		else:
+			# X = np.load(self.path + 'x_prime.npy', allow_pickle = True)	
+			# X_prime = np.load(self.path + 'x_prime.npy', allow_pickle = True)
+			# isFeatureReal = np.load(self.path + 'feature_information.npy', allow_pickle = True)
+
+			X_train = np.load(self.path + 'Train/X.npy', allow_pickle = True)
+			X_test = np.load(self.path + 'Test/X.npy', allow_pickle = True)
+
+			X_prime_train = np.load(self.path + 'Train/X_prime.npy', allow_pickle = True)
+			X_prime_test = np.load(self.path + 'Test/X_prime.npy', allow_pickle = True)
+
+			isFeatureReal_train = np.load(self.path + 'Train/feature_information.npy', allow_pickle = True)
+			isFeatureReal_test = np.load(self.path + 'Test/feature_information.npy', allow_pickle = True)
+
+
+			# print(isFeatureReal)
+			# print(isFeatureReal.shape)
+			# print("Total missing = ", np.sum(isFeatureReal==0))
+
+			# print(X_prime)
+			# print(X_prime.shape)
+
+		return X_train, X_test, X_prime_train, X_prime_test, isFeatureReal_train, isFeatureReal_test
 
 
 	def prepareMissingData(self, df, nMissingMax = 3):
+		pd.options.mode.chained_assignment = None
 
 		m, k = df.shape
 		isFeatureReal = pd.DataFrame(1, columns = df.columns, index = df.index)
@@ -69,7 +115,7 @@ class Data:
 		continuousColList = [x for x in self.columnList if x not in self.categoricalColumnList]
 
 		# Iterating each row
-		for idx in range(m):
+		for idx in tqdm(range(m)):
 
 			row = df.iloc[idx]
 
@@ -131,4 +177,10 @@ class Data:
 
 
 data = Data('../Data/')
-data.getData()
+X_train, X_test, X_prime_train, X_prime_test, isFeatureReal_train, isFeatureReal_test = data.getData(newData = False)
+print("X train shape = ", X_train.shape)
+print("X test shape = ", X_test.shape)
+print("X prime train shape = ", X_prime_train.shape)
+print("X prime test shape = ", X_prime_test.shape)
+print("feature information train shape = ", isFeatureReal_train.shape)
+print("feature information test shape = ", isFeatureReal_test.shape)
