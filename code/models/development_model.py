afaf11ab431f61e2c25976c5738c03354e44eadb
keya-desai <keyadesai97@gmail.com>
2020-12-16 02:42:08 +0530

    Update development model

--- code/models/development_model.py
+++ code/models/development_model.py
@@ -42,10 +42,12 @@ def linear_regression_model(X, y, learning_rate, epochs):
     return w, b
 
 
+
+
 def dev_model(X_prime_train, X_prime_test, isFeatureReal_train, isFeatureReal_test, baseline_model_obj):
 
     # Trying out with missing age column 
-    col_to_predict = 
+    col_to_predict = 100
     nFeatures = len(X_prime_train[0])
     # List of columns index to fill using baesline methods
     col_idx_list = [i for i in range(nFeatures) if i != col_to_predict]
@@ -65,6 +67,8 @@ def dev_model(X_prime_train, X_prime_test, isFeatureReal_train, isFeatureReal_te
     # print(len(isFeatureReal_train_temp[isFeatureReal_train_temp[:, col_to_predict] == 1]))
     # print(x_training.shape)
 
+    print(x_training.head())
+
     m_training, k_training = x_training.shape
     dummy_y = np.array([0] * m_training)
     w, b = linear_regression_model(np.array(x_training), dummy_y, learning_rate = 0.00001, epochs = 10000)
@@ -73,19 +77,23 @@ def dev_model(X_prime_train, X_prime_test, isFeatureReal_train, isFeatureReal_te
     isFeatureReal_test = pd.DataFrame(isFeatureReal_test)
 
 
-    new_w = np.delete(w, col_to_predict)
-    for idx, row in isFeatureReal_train.loc[isFeatureReal_train[col_to_predict] == 0].iterrows(): 
-        # print(col_to_predict)
-        # print(row)
-        # print(len(row))
-        # print(row.drop(col_to_predict))
-        # new_row = 
-        new_row = row.drop(col_to_predict)
-        pred = - (np.dot(new_w, new_row) + b )/w[col_to_predict]
-        X_prime_train[col_to_predict].iloc[idx] = pred
+    print(np.mean(np.matmul(np.array(x_training), w) + b))
 
     new_w = np.delete(w, col_to_predict)
-    for idx, row in isFeatureReal_test.loc[isFeatureReal_test[col_to_predict] == 0].iterrows():
+    cnt = 0
+    for idx, _ in isFeatureReal_train.loc[isFeatureReal_train[col_to_predict] == 0].iterrows(): 
+        row = X_prime_train.iloc[idx]
+        new_row = row.drop(col_to_predict)
+        pred = - (np.dot(new_w, new_row) + b )/w[col_to_predict]
+        print(idx, pred)
+        X_prime_train[col_to_predict].iloc[idx] = pred
+        cnt += 1
+        if cnt > 5 : break
+
+
+    new_w = np.delete(w, col_to_predict)
+    for idx, _ in isFeatureReal_test.loc[isFeatureReal_test[col_to_predict] == 0].iterrows():
+        row = X_prime_test.iloc[idx]
         new_row = row.drop(col_to_predict)
         pred = - (np.dot(new_w, new_row) + b )/w[col_to_predict]
         X_prime_test[col_to_predict].iloc[idx] = pred    
